4e9141594cf8573ca89d433a89b47f495ba2077f
js905531 <157529761+js905531@users.noreply.github.com>
2024-09-13 19:49:01 -0400

    Week 2 1

--- src/main/java/Q1WeirdFunction.java
+++ src/main/java/Q1WeirdFunction.java
@@ -4,11 +4,35 @@ public class Q1WeirdFunction {
 
     public static int fRecursive(int n) {
         // TODO: Implement the recursive function
-        return 0;
+        if (n<=2){
+            return n;
+        }
+        else if (n==3){
+            return 4;
+        }
+        else{
+            return n + fRecursive(n-1) + fRecursive(n-2);
+        }
     }
 
     public static int fIterative(int n) {
         // TODO: Implement the iterative function
-        return 0;
+        if (n<=2){
+            return n;
+        }
+        else if (n==3){
+            return 4;
+        }
+        else{
+            int [] f = new int[n+1];
+            f[0] = 0;
+            f[1] = 1;
+            f[2] = 2;
+            f[3] = 4;
+            for (int i=4;i<=n;i++){
+                f[i] = n + f[i-1] + f[i-2];
+            }
+            return f[n];
+        }
     }
 }
--- src/main/java/Q4ReverseString.java
+++ src/main/java/Q4ReverseString.java
@@ -3,7 +3,9 @@
 public class Q4ReverseString {
 
     public static String reverse(String s) {
-        // TODO: Implement the recursive function to reverse the string
-        return "";
+        if (s.length() <= 1) {
+            return s;
+        }
+        return s.charAt(s.length() - 1) + reverse(s.substring(0, s.length() - 1));
     }
 }
